8eae1602786af698fd54148aa5635c5ea62dad03
[dyad] <rafisha92@gmail.com>
2025-12-20 20:58:27 +0800

    [dyad] Fix ENOTSUP error - wrote 1 file(s)

--- server/index.ts
+++ server/index.ts
@@ -64,7 +64,7 @@ app.use((req, res, next) => {
   server.listen({
     port,
     host: "0.0.0.0",
-    reusePort: true,
+    reusePort: false,
   }, () => {
     log(`serving on port ${port}`);
   });
